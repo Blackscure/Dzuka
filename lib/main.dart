fd7a7598df32f15694d2aab0c84f8fe36b01d2c5
blackscure <Wekesabuyahi@gmail.com>
2021-05-08 11:43:35 +0300

    duration for screen splash set to 5 secs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() => runApp(MaterialApp(
       theme:
@@ -14,8 +15,14 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5),()=> print("next activity"))
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: Stack(
         fit: StackFit.expand,
         children: <Widget>[
@@ -40,34 +47,40 @@ class _SplashScreenState extends State<SplashScreen> {
                           size: 50.0,
                         ),
                       ),
-                      Padding(padding: EdgeInsets.only(top: 10.0),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
                       ),
                       Text(
-                        "Dzuka",style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold),
-                        ),
+                        "Dzuka",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold),
+                      ),
                     ],
                   ),
                 ),
               ),
-              Expanded(flex: 1,
+              Expanded(
+                flex: 1,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     CircularProgressIndicator(),
-                    Padding(padding: EdgeInsets.only(top: 20.0),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
                     ),
-                    Text("Dzuka for Everyone \n Everyone for Dzuka",
-                    style: TextStyle(color: Colors.white,
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold),
+                    Text(
+                      "Dzuka for Everyone \n Everyone for Dzuka",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold),
                     ),
                   ],
                 ),
               )
-          ],
+            ],
           ),
         ],
       ),
